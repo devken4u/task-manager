b7b00bbf5bff7008761ee4be78573a1f8acd7c39
devken4u <devken4u@gmail.com>
2024-12-19 12:30:36 +0800

    updated

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://task-manager-tan-nu.vercel.app",
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
