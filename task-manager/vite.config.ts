a73a961b2af98643fa4ff85513be483e737a1467
devken4u <devken4u@gmail.com>
2024-12-19 16:24:52 +0800

    updated3

--- task-manager/vite.config.ts
+++ task-manager/vite.config.ts
@@ -1,26 +1,26 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import "dotenv/config";
-
-// const backendUrl = process.env.BACKEND_BASE_URL;
-// console.log(backendUrl);
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: backendUrl,
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import "dotenv/config";
 
+const backendUrl = process.env.BACKEND_BASE_URL;
+console.log(backendUrl);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: backendUrl,
+        changeOrigin: true,
+      },
+    },
+  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
