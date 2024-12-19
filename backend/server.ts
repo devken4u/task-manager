15a8181f9ff29947a9588591f03b878519954b68
devken4u <devken4u@gmail.com>
2024-12-19 13:15:11 +0800

    updated

--- backend/server.ts
+++ backend/server.ts
@@ -9,14 +9,7 @@ import { Request, Response } from "express";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://task-manager-frontend-eight-omega.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
